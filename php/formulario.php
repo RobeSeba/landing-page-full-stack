55bab9ccedc83bda45c9994720414cfc18a481b7
Roberto Sebastian <roberto_m_ll@hotmail.com>
2025-09-06 18:36:57 -0500

    se creo la clase conexion a la base de datos

--- php/formulario.php
+++ php/formulario.php
@@ -1,10 +1,13 @@
 <?php
-  if (isset($_GET['NombreInput']) && isset($_GET['EmailInput'])) {
+// comprobando que los datos lleguen a mi formulario
+  if (isset($_GET['NombreInput']) && isset($_GET['EmailInput']) && isset($_GET['ComentarioInput'])) {
     $nombre = $_GET['NombreInput'];
     $email = $_GET['EmailInput'];
+    $comentario=$_GET['ComentarioInput'];
     echo "Tu nombre es: " . htmlspecialchars($nombre) . "<br>";
-    echo "Tu correo es: " . htmlspecialchars($email);
+    echo "Tu correo es: " . htmlspecialchars($email) . "<br>";
+    echo "tu comentario es ". htmlspecialchars($comentario);
   } else {
-    echo "Faltan datos en el formulario.";
+    echo "No se completaron los campos en el formulario";
   }
 ?>
