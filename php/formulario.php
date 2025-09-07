28fc9f6ff3223164e49d1e6b8b8b938210589e42
Roberto Sebastian <roberto_m_ll@hotmail.com>
2025-09-06 20:03:44 -0500

    se creeo logia.php para los metodos crud y se actualizo formulario.php

--- php/formulario.php
+++ php/formulario.php
@@ -1,12 +1,24 @@
 <?php
+require_once "logica.php";
+$registro = new Registros();
 // comprobando que los datos lleguen a mi formulario
   if (isset($_GET['NombreInput']) && isset($_GET['EmailInput']) && isset($_GET['ComentarioInput'])) {
     $nombre = $_GET['NombreInput'];
     $email = $_GET['EmailInput'];
     $comentario=$_GET['ComentarioInput'];
-    echo "Tu nombre es: " . htmlspecialchars($nombre) . "<br>";
-    echo "Tu correo es: " . htmlspecialchars($email) . "<br>";
-    echo "tu comentario es ". htmlspecialchars($comentario);
+    // echo "Tu nombre es: " . htmlspecialchars($nombre) . "<br>";
+    // echo "Tu correo es: " . htmlspecialchars($email) . "<br>";
+    // echo "tu comentario es ". htmlspecialchars($comentario);
+
+    $resultado = $registro->insertar($nombre, $email, $comentario);
+
+    if ($resultado === true) {
+        header("Location:../index.html");
+    } else {
+        echo "<strong>❌ Error:</strong> " . htmlspecialchars($resultado);
+    }
+
+
   } else {
     echo "No se completaron los campos en el formulario";
   }
