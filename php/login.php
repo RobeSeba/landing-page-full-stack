bb3d6b7f4bdd6688afe08ea3a842c7eadef7be96
Roberto Sebastian <roberto_m_ll@hotmail.com>
2025-10-02 00:21:10 -0500

    se esta trabajando con composer para instalar las dependencias de jwt,  se creo el dashboard en pages. ya se creo el codigo para validar el usuario, la bd usa un hash, en pruebas esta la contraseña que he usado en la bd

--- php/login.php
+++ php/login.php
@@ -1,11 +1,23 @@
 <?php
     require_once "conexion.php";
+    require_once "logica.php";
 
+    $registro = new Registros();
     if(isset($_POST['EmailInput']) && isset($_POST['PasswordInput'])){
         $email=$_POST['EmailInput'];
         $password=$_POST['PasswordInput'];
-        echo" tu email es ". htmlspecialchars($email) . "<br>";
-        echo" tu contraseña es ". htmlspecialchars($password)."<br>";
+        
+       $respuesta= $registro -> validarUsuario($email,$password);
+       if ($respuesta["success"]) {
+    // Guardar token en cookie (dura 1 hora)
+        setcookie("jwt_token", $respuesta["token"], time() + 3600, "/", "", false, true);
+        header("Location: ../pages/dashboard.php");
+        exit();
+        } else {
+            echo $respuesta["message"];
+        }
+       //echo json_encode($token);
+
 
     }
 ?>
